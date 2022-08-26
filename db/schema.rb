a22e40c05db65f8f96006febfbd2b2525d94034f
tjhaines-cap <tjhaines@eckerd.edu>
2022-08-26 16:26:30 -0600

    Feat: create locations table
    Co-authored-by: Andrew King <dev.andrewking@gmail.com>
    Co-authored-by: Tyler Ross <tross020890@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_26_221528) do
+ActiveRecord::Schema.define(version: 2022_08_26_222502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "locations", force: :cascade do |t|
+    t.string "street_address"
+    t.string "city"
+    t.string "state"
+    t.string "zipcode"
+    t.string "type"
+    t.integer "scariness_level"
+    t.string "description"
+    t.string "start_time"
+    t.string "end_time"
+    t.string "image"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_locations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -21,4 +40,5 @@ ActiveRecord::Schema.define(version: 2022_08_26_221528) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "locations", "users"
 end
