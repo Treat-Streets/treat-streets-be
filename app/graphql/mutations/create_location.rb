7698045fb65627426ce07180f44131630731f927
tjhaines-cap <tjhaines@eckerd.edu>
2022-08-27 12:09:09 -0600

    Feat: create user then location in mutation
    Co-authored-by: Andrew <dev.andrewking@gmail.com>
    Co-authored-by: Tyler <tross020890@gmail.com>

--- app/graphql/mutations/create_location.rb
+++ app/graphql/mutations/create_location.rb
@@ -1,4 +1,4 @@
-class Mutation::CreateLocation < Mutations::BaseMutation
+class Mutations::CreateLocation < Mutations::BaseMutation
   argument :street_address, String, required: true
   argument :city, String, required: true
   argument :state, String, required: true
@@ -9,21 +9,25 @@ class Mutation::CreateLocation < Mutations::BaseMutation
   argument :start_time, String, required: true
   argument :end_time, String, required: true
   argument :image, String, required: false
+  argument :email, String, required: true
 
   field :location, Types::LocationType, null: false
   field :errors, [String], null: false
 
-  def resolve(street_address:, city:, state:, zipcode:, location_type:, description:, scariness_level:, start_time:, end_time:, image:)
-    location = Location.new(street_address: street_address, city: city, state: state, zipcode: zipcode, location_type: location_type, description: description, scariness_level: scariness_level, start_time: start_time, end_time: end_time, image: image)
+  def resolve(street_address:, city:, state:, zipcode:, location_type:, description:, scariness_level:, start_time:, end_time:, image:, email:)
+    user = User.find_or_create_by(email: email)
+    location = Location.new(street_address: street_address, city: city, state: state, zipcode: zipcode, location_type: location_type, description: description, scariness_level: scariness_level, start_time: start_time, end_time: end_time, image: image, user: user)
   
-    if (location.save)
+    if location.save
       {
         location: location,
         errors: []
       }
     else
-      location: nil,
-      errors: user.errors.full_messages
+      {
+        location: nil,
+        errors: user.errors.full_messages
+      }
     end
   end
 end
