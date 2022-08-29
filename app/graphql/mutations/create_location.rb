bd463c569c608b10de440bf7be8e8150b175e84e
Tyler Ross <tross020890@gmail.com>
2022-08-29 12:25:41 -0500

    Fix: change description to required: false
    Co-authored-by Thomas <tjhaines@eckerd.edu>
    Co-authored-by Andrew <dev.andrewking@gmail.com>

--- app/graphql/mutations/create_location.rb
+++ app/graphql/mutations/create_location.rb
@@ -4,7 +4,7 @@ class Mutations::CreateLocation < Mutations::BaseMutation
   argument :state, String, required: true
   argument :zipcode, String, required: true
   argument :location_type, String, required: true
-  argument :description, String, required: true
+  argument :description, String, required: false
   argument :scariness_level, Integer, required: true
   argument :start_time, String, required: true
   argument :end_time, String, required: true
@@ -17,7 +17,7 @@ class Mutations::CreateLocation < Mutations::BaseMutation
   def resolve(street_address:, city:, state:, zipcode:, location_type:, description:, scariness_level:, start_time:, end_time:, image:, email:)
     user = User.find_or_create_by(email: email)
     location = Location.new(street_address: street_address, city: city, state: state, zipcode: zipcode, location_type: location_type, description: description, scariness_level: scariness_level, start_time: start_time, end_time: end_time, image: image, user: user)
-  
+
     if location.save
       {
         location: location,
