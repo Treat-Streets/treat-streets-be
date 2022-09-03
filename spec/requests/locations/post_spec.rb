b9a0b01f5bbd67b41e261775ff11f83be59e8146
tjhaines-cap <tjhaines@eckerd.edu>
2022-09-03 15:33:43 -0600

    Ref: add context and description blocks to post tests

--- spec/requests/locations/post_spec.rb
+++ spec/requests/locations/post_spec.rb
@@ -4,36 +4,36 @@ module Mutations
   RSpec.describe "Posts", :vcr do
 
     describe "POST /graphql" do
-      describe "happy path" do
-        it "returns the location posted" do
-          expect do
-            post '/graphql', params: { query: query() }
-          end.to change { Location.count }.by(1)                       
+      context "success" do
+        describe "createLocation mutation" do
+          it "returns the location posted" do
+            expect do
+              post '/graphql', params: { query: query() }
+            end.to change { Location.count }.by(1)                       
+          end
         end
 
-        it 'returns all locations' do 
-          post '/graphql', params: { query: query }
-          post '/graphql', params: { query: query }
+        describe "locations query" do
+          it 'returns all locations' do 
+            post '/graphql', params: { query: query }
+            post '/graphql', params: { query: query }
 
-          post '/graphql', params: { query: get_query }
-          
-          json_response = JSON.parse(response.body, symbolize_names: true)
-          expect(json_response[:data][:locations].count).to eq(2)
+            post '/graphql', params: { query: get_query }
+            
+            json_response = JSON.parse(response.body, symbolize_names: true)
+            expect(json_response[:data][:locations].count).to eq(2)
+          end
         end
       end
 
-      describe "sad path" do
-        it "returns 'Invalid address' error if address is invalid" do
-          post '/graphql', params: { query: invalid_address_query() }
-          json_response = JSON.parse(response.body, symbolize_names: true)
-          expect(json_response[:data][:createLocation][:errors][0]).to eq('Invalid address')
+      context "failure" do
+        describe "createLocation mutation" do
+          it "returns 'Invalid address' error if address is invalid" do
+            post '/graphql', params: { query: invalid_address_query() }
+            json_response = JSON.parse(response.body, symbolize_names: true)
+            expect(json_response[:data][:createLocation][:errors][0]).to eq('Invalid address')
+          end
         end
-        # it "returns 'Invalid address' error if address is invalid" do
-        #   post '/graphql', params: { query: invalid_address_query() }
-        #   json_response = JSON.parse(response.body, symbolize_names: true)
-        #   binding.pry
-        #   expect(json_response[:errors][0][:message]).to eq('Cannot return null for non-nullable field CreateLocationPayload.location')
-        # end
       end
     end
 
