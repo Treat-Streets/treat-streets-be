71dcf5d0859b0c75a2e2f1258dfeb4f4d60e8804
Tyler Ross <tross020890@gmail.com>
2022-09-06 11:55:29 -0500

    Ref: add description, image url to seed data, add PM to times

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,24 +20,24 @@ cities.each do |city|
                     state: location[:location][:state],
                     zipcode: location[:location][:zip_code],
                     location_type: "Business",
-                    start_time: "3:00",
-                    end_time: "7:00",
+                    start_time: "4:00 PM",
+                    end_time: "10:00 PM",
                     latitude: location[:coordinates][:latitude],
                     longitude: location[:coordinates][:longitude],
                     scariness_level: 2,
-                    image: "url.com",
+                    image: "https://res.cloudinary.com/drexo2l5j/image/upload/v1662338245/j44lo7idesuaxuta0iwz.jpg",
+                    description: "Happy Halloween!",
                     user: user)
   end
 end
 
-Location.create!(street_address: "43 Sommerset Circle", city: "Englewood", state:"CO",
-   zipcode: "80111", location_type: "Home", start_time: "3:00", end_time: "7:00", latitude: 39.614408 , longitude: -104.892167, scariness_level: 2, image: "url.com", user: user)
-Location.create!(street_address: "3815 S Bannock St", city: "Englewood", state:"CO",
-   zipcode: "80110", location_type: "Home", start_time: "3:00", end_time: "7:00", latitude: 39.647048, longitude: -104.990167, scariness_level: 6, image: "scaryhouses.com", user: user1)
-Location.create!(street_address: "3822 S Bannock St", city: "Englewood", state:"CO",
-   zipcode: "80110", location_type: "Home", start_time: "3:00", end_time: "7:00", latitude: 39.64680, longitude: -104.98997, scariness_level: 3, image: "scaryhouses.com", user: user2)
-Location.create!(street_address: "3865 S Bannock St", city: "Englewood", state:"CO",
-   zipcode: "80110", location_type: "Home", start_time: "5:00", end_time: "8:00", latitude: 39.64649, longitude: -104.99025, scariness_level: 3, image: "scaryhouses.com", user: user2)
-Location.create!(street_address: "3890 S Bannock St", city: "Englewood", state:"CO",
-   zipcode: "80110", location_type: "Home", start_time: "5:00", end_time: "8:00", latitude: 39.64610, longitude: -104.98997, scariness_level: 3, image: "scaryhouses.com", user: user2)
-
+Location.create!(street_address: "43 Sommerset Circle", city: "Englewood", state:"CO", description: "Happy Halloween!",
+   zipcode: "80111", location_type: "Home", start_time: "4:00 PM", end_time: "7:00 PM", latitude: 39.614408 , longitude: -104.892167, scariness_level: 2, image: "https://res.cloudinary.com/drexo2l5j/image/upload/v1662338245/j44lo7idesuaxuta0iwz.jpg", user: user)
+Location.create!(street_address: "3815 S Bannock St", city: "Englewood", state:"CO", description: "Happy Halloween!",
+   zipcode: "80110", location_type: "Home", start_time: "4:00 PM", end_time: "7:00 PM", latitude: 39.647048, longitude: -104.990167, scariness_level: 6, image: "https://res.cloudinary.com/drexo2l5j/image/upload/v1662338245/j44lo7idesuaxuta0iwz.jpg", user: user1)
+Location.create!(street_address: "3822 S Bannock St", city: "Englewood", state:"CO", description: "Happy Halloween!",
+   zipcode: "80110", location_type: "Home", start_time: "4:00 PM", end_time: "7:00 PM", latitude: 39.64680, longitude: -104.98997, scariness_level: 3, image: "https://res.cloudinary.com/drexo2l5j/image/upload/v1662338245/j44lo7idesuaxuta0iwz.jpg", user: user2)
+Location.create!(street_address: "3865 S Bannock St", city: "Englewood", state:"CO", description: "Happy Halloween!",
+   zipcode: "80110", location_type: "Home", start_time: "5:00 PM", end_time: "8:00 PM", latitude: 39.64649, longitude: -104.99025, scariness_level: 3, image: "https://res.cloudinary.com/drexo2l5j/image/upload/v1662338245/j44lo7idesuaxuta0iwz.jpg", user: user2)
+Location.create!(street_address: "3890 S Bannock St", city: "Englewood", state:"CO", description: "Happy Halloween!",
+   zipcode: "80110", location_type: "Home", start_time: "5:00 PM", end_time: "8:00 PM", latitude: 39.64610, longitude: -104.98997, scariness_level: 3, image: "https://res.cloudinary.com/drexo2l5j/image/upload/v1662338245/j44lo7idesuaxuta0iwz.jpg", user: user2)
